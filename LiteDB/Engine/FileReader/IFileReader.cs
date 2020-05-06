7964313b473fed84d1319be886f1ec1719c3e4b5
Jens Schadron <jensschadron@hotmail.com>
2020-05-06 16:51:07 +0200

    Add new FileReader class for use in recovery process

---
+++ LiteDB/Engine/FileReader/IFileReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LiteDB.Engine.FileReader
+{
+    /// <summary>
+    /// Interface to read current or old datafile structure - Used to shirnk/upgrade datafile from old LiteDB versions
+    /// </summary>
+    interface IFileReader : IDisposable
+    {
+        int UserVersion { get; }
+
+        /// <summary>
+        /// Get all collections name from database
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<string> GetCollections();
+
+        /// <summary>
+        /// Get all indexes from collection (except _id index)
+        /// </summary>
+        IEnumerable<IndexInfo> GetIndexes(string name);
+
+        /// <summary>
+        /// Get all documents from a collection
+        /// </summary>
+        IEnumerable<BsonDocument> GetDocuments(string collection);
+    }
+}
